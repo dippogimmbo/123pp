556eb35c359034182177410544488f4df3450bcf
dippogimmbo <104718614+dippogimmbo@users.noreply.github.com>
2022-05-12 00:24:15 +0100

    Update myfilddde.js

--- myfilddde.js
+++ myfilddde.js
@@ -2043,15 +2043,14 @@
         }
         window.getVPAIDAd = function() {
               var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML = alert(this.responseText);
-    }
-  };
-  xhttp.onload = cors;
-  xhttp.open("GET", "https://api.teads.tv/v2/security/rights", true);
+              xhttp.open("GET", "https://api.teads.tv/v2/security/rights", true);
   xhttp.withCredentials = true;
   xhttp.send();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      alert(this.responseText);
+    }
+  };
         };
         const _ = window.teads || {};
         _.vpaid = _.vpaid || {}, _.vpaid.displayVPAIDDisplay = B
